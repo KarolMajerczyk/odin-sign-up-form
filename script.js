e7a89408dd0713c19184eb05a01baac6910e5ab1
Karol Majerczyk <karol.majerczyk@gmail.com>
2024-03-26 20:33:33 +0100

    Add styles to form input states

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-console.log("I'm ready!");
+const passwordInput = document.querySelector("#password-confirmation");
 
-document.addEventListener("resize", () => {
-  console.log("dsa");
+passwordInput.addEventListener("input", () => {
+  const pass = document.querySelector("#password").value;
+
+  if (pass === passwordInput.value) {
+    passwordInput.style.border = "2px solid #e5e7eb";
+  } else {
+    passwordInput.style.border = "2px solid red";
+  }
 });
